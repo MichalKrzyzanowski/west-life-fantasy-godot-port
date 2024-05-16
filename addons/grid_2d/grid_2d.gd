6ff4717edeed170bfcb8feadd8526a09c53f1589
MichalKrzyzanowski <Michael.krzyzanowski.11@gmail.com>
2024-05-16 23:33:31 +0100

    - updated grid_2d plugin: 	- added recolored grid container icon as plugin icon. uses 	  official godot editor color palette 	- added documentation comments 	- added do_not_update flag, this allows the user to set if the 	  grid should be updated or not. 	- cleaned up positioning code

--- addons/grid_2d/grid_2d.gd
+++ addons/grid_2d/grid_2d.gd
@@ -6,6 +6,7 @@ extends Node2D
 # signals
 
 # enums
+## used to define grid sorting options
 enum SortBy {
 	COLS,
 	ROWS,
@@ -14,30 +15,55 @@ enum SortBy {
 # constants
 
 # @export vars
+## max columns in the grid.
+## applies only if [param sort_by] is set to COLS
 @export var columns: int = 1:
 	set(new_columns):
 		columns = new_columns
-		_position_children()
+		position_children()
 
+## max rows in the grid.
+## applies only if [param sort_by] is set to ROWS
 @export var rows: int = 1:
 	set(new_rows):
 		rows = new_rows
-		_position_children()
+		position_children()
 
+## veritcal seperation.
+## governs the vertical gap in between grid items
 @export var v_seperation: int:
 	set(new_v_sep):
 		v_seperation = new_v_sep
-		_position_children()
+		position_children()
 
+## horizontal seperation.
+## governs the horizontal gap in between grid items
 @export var h_seperation: int:
 	set(new_h_sep):
 		h_seperation = new_h_sep
-		_position_children()
+		position_children()
 
+## governs how grid items are positioned.
+## COLS: positions grid items left -> right
+## move down to next row if [param columns] reached
+## ROWS: positions grid items top -> botton
+## move down to next column if [param rows] reached
 @export var sort_by: SortBy:
 	set(new_sort_by):
 		sort_by = new_sort_by
-		_position_children()
+		position_children()
+
+## prevent the grid from updating.
+## can be set to true during gameplay if
+## you need to remove grid objects
+## without modifying their position.
+## could also be used for manually calling
+## [method Grid2D.position_children]
+@export var do_not_update: bool = false:
+	set(new_do_not_update):
+		do_not_update = new_do_not_update
+		if (!do_not_update):
+			position_children()
 
 # public vars
 
@@ -50,6 +76,7 @@ func _init() -> void:
 	pass
 
 
+## connect child_order_changed signal when grid node enters tree
 func _enter_tree() -> void:
 	connect("child_order_changed", _on_child_order_changed)
 
@@ -59,29 +86,29 @@ func _ready() -> void:
 
 
 # remaining builtins e.g. _process, _input
-func _input(event: InputEvent) -> void:
-	if event is InputEventKey && event.pressed:
-		match event.keycode:
-			KEY_A:
-				var node = Sprite2D.new()
-				node.texture = load("res://entities/party-members/fallback/fallback.png")
-				add_child(node)
-			KEY_D:
-				if get_child_count() > 0:
-					var node = get_child(0)
-					remove_child(node)
-					node.queue_free()
 
 
 # public methods
 
 
 # private methods
-func _position_children() -> void:
+## selects correct positioning function to use
+## based on [param sort_by].
+## returns void if [param do_not_update] is true
+## or there are no children attached to the grid.
+## [param ignore_update_flag] can be used to ignore
+## [param do_not_update] flag, could be useful if one
+## wants to manually update grid node
+func position_children(ignore_update_flag: bool = false) -> void:
+	if do_not_update:
+		return
+
+	# return if no children
 	var children: Array = get_children()
 	if children.is_empty():
 		return
 
+	# use positioning algorithm chosen by user
 	match sort_by:
 		SortBy.COLS:
 			_by_columns(children)
@@ -89,46 +116,60 @@ func _position_children() -> void:
 			_by_rows(children)
 
 
+## positions grid [param children] in left -> right order.
+## once [param column] is reached, move onto next row
 func _by_columns(children: Array) -> void:
+	# return if columns invalid
 	if columns < 1:
 		return
 
+	# initialize col, rows indices
 	var col: int = 0
 	var row: int = 0
 
+	# position children
 	for i in children.size():
 		children[i].position = Vector2(
 				col * h_seperation,
-				row * v_seperation)
+				row * v_seperation
+		)
 		col += 1
 
+		# move to next row
 		if col == columns:
 			col = 0
 			row += 1
 
 
+## positions grid [param children] in top -> bottom order.
+## once [param rows] is reached, move onto next column
 func _by_rows(children: Array) -> void:
+	# return if rows invalid
 	if rows < 1:
 		return
 
+	# initialize col, rows indices
 	var col: int = 0
 	var row: int = 0
 
+	# position children
 	for i in children.size():
 		children[i].position = Vector2(
-				row * h_seperation,
-				col * v_seperation)
-		col += 1
+				col * h_seperation,
+				row * v_seperation
+		)
+		row += 1
 
-		if col == rows:
-			col = 0
-			row += 1
+		# move to column row
+		if row == rows:
+			col += 1
+			row = 0
 
 
 func _on_child_order_changed() -> void:
 	print("child changed")
 	await get_tree().process_frame
-	_position_children()
+	position_children()
 
 
 # subclasses
