3c9e638a565e0491962a436e57823c5af55f8f56
MichalKrzyzanowski <Michael.krzyzanowski.11@gmail.com>
2024-07-02 10:11:21 +0100

    - general changes: 	- added code to player that connects to current fader 	  animation_finished and animation_begin signals

--- entities/overworld-player/overworld_movement.gd
+++ entities/overworld-player/overworld_movement.gd
@@ -5,22 +5,35 @@ signal on_enemy_hit(body: Node2D, on_advantage: bool)
 
 @export var speed: float = 5.0
 
-var combat_manager_ref: Node
+var allow_movement: bool = true
 
 @onready var sprite := get_node("Sprite2D") as Sprite2D
 @onready var camp_menu := get_node("UI/CampMenu") as Control
 @onready var camera := $Camera2D as Camera2D
 
 
+func _ready() -> void:
+	var fader = get_parent().get_node("Fader")
+	if fader:
+		print("found fader")
+		fader.animation_started.connect(_on_fade_begin)
+		fader.animation_finished.connect(_on_fade_end)
+
+
 ## physics process function that handles player movement,
 ## player facing the direction of movement, and collisions
 func _physics_process(_delta: float) -> void:
+	if !allow_movement:
+		return
+
 	var input_dir = Input.get_vector("left", "right", "up", "down")
 	velocity = input_dir * speed
 
 	_flip_sprite(input_dir)
 	var collision = move_and_collide(velocity)
-	if collision: # get slide velocity and half it so that sliding along a wall is slower
+
+	if collision:
+		# get slide velocity and half it so that sliding along a wall is slower
 		velocity = velocity.slide(collision.get_normal()) / 2.0
 		move_and_collide(velocity)
 
@@ -55,3 +68,11 @@ func _flip_sprite(input_dir: Vector2):
 		sprite.flip_h = false
 	if input_dir.x < 0:
 		sprite.flip_h = true
+
+
+func _on_fade_begin(_anim_name: String) -> void:
+	allow_movement = false
+
+
+func _on_fade_end(_anim_name: String) -> void:
+	allow_movement = true
